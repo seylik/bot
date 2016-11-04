6636912ecb5659e429d07b42275f3371bb965ec9
seylik <k.ilyes@live.fr>
2016-11-04 23:01:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-app.post('/webhook/', function (req, res) {
-    messaging_events = req.body.entry[0].messaging
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
-        if (event.message && event.message.text) {
-            text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-        }
-    }
-    res.sendStatus(200)
-})
-
-var token = "<PAGE_ACCESS_TOKEN>"
+bot
