75d530880a157591febc55f71779620baf08afed
seylik <k.ilyes@live.fr>
2016-11-04 22:29:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-app.post('/issue-updated', addon.authenticate(), function (req, res) {
-        var httpClient = addon.httpClient(req);
- 
-        var key = req.body.issue.key;
- 
-        var array = [];
-        httpClient.get('/rest/api/2/issue/' + key + '/properties/myproperty', function(err, res, body){
- 
-            // Push properties in array
- 
-            var url = '/rest/api/2/issue/' + key + '/properties/myproperty';
-            var args = {
-                entities: array
-            };
- 
-            console.log("TEST!");
- 
-            httpClient.put({url: url, json: args}, function(err, res, body) {
-                console.log("Request");
-                console.log(err);
-                console.log(body);
-            });
- 
-        });
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
     }
-);
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
