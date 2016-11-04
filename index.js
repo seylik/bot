bece6acea2ca513293b4e1d0b301a3cf639a3a6d
seylik <k.ilyes@live.fr>
2016-11-04 22:35:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ app.post('/webhook/', function (req, res) {
 })
 
 var token = "<EAAB3xTiHDMEBAJUxxGxciLRAriXJg6vSoUI0VfViRMMzLYwF8pCJepsEy5NbS19lX1eZCW7h7zFC9WgscZBvwUXTFbhhVcZAk5H5xhX6FcXbt3f07vaKD4wl0V8pKLF32Lr7ZBIS2MVjzvxNjPTePHsZCwZAN5Q1ZCtpZAH9olKIXgZDZD>"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
