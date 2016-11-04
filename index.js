d57b62afd88a57ec14983de2bc02d71e14c6df1d
seylik <k.ilyes@live.fr>
2016-11-04 22:44:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,28 @@
-app.post('/webhook/', function (req, res) {
-    messaging_events = req.body.entry[0].messaging
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
-        if (event.message && event.message.text) {
-            text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-        }
-    }
-    res.sendStatus(200)
+'use strict'
+const http = require('http')
+const Bot = require('messenger-bot')
+
+let bot = new Bot({
+  token: 'EAAB3xTiHDMEBAJUxxGxciLRAriXJg6vSoUI0VfViRMMzLYwF8pCJepsEy5NbS19lX1eZCW7h7zFC9WgscZBvwUXTFbhhVcZAk5H5xhX6FcXbt3f07vaKD4wl0V8pKLF32Lr7ZBIS2MVjzvxNjPTePHsZCwZAN5Q1ZCtpZAH9olKIXgZDZD',
+  verify: 'verify_me'
 })
 
-var token = "<EAAB3xTiHDMEBAJUxxGxciLRAriXJg6vSoUI0VfViRMMzLYwF8pCJepsEy5NbS19lX1eZCW7h7zFC9WgscZBvwUXTFbhhVcZAk5H5xhX6FcXbt3f07vaKD4wl0V8pKLF32Lr7ZBIS2MVjzvxNjPTePHsZCwZAN5Q1ZCtpZAH9olKIXgZDZD>"
+bot.on('error', (err) => {
+  console.log(err.message)
+})
 
-function sendTextMessage(sender, text) {
-    messageData = {
-        text:text
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
+bot.on('message', (payload, reply) => {
+  let text = payload.message.text
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err
+
+    reply({ text }, (err) => {
+      if (err) throw err
+
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
     })
-}
+  })
+})
+
+http.createServer(bot.middleware()).listen(3000)
